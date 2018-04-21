5fcc0e999bfa23a133dadec1fa7552d28694934b
jake cabrera <jakestengercabrera@gmail.com>
2018-04-21 15:32:45 -0700

    Added Player class Added InputHandler class

--- core/src/com/fireball/game/FireballTheGame.java
+++ core/src/com/fireball/game/FireballTheGame.java
@@ -9,71 +9,42 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class FireballTheGame extends ApplicationAdapter {
-	Animation<TextureRegion> walkAnimation;
-	Animation<TextureRegion> spellAnimation;
 	Animation<TextureRegion> candleAnimation;
 	Animation<TextureRegion> fireballAnimation;
-	Texture walkSheet;
-	Texture spellSheet;
 	Texture litCandleSheet;
 	Texture fireballSheet;
 
-	TextureRegion player;
+	Player player;
 
 	SpriteBatch spriteBatch;
 
 	float stateTime;
-	float playerStateTime;
 	float fireStateTime;
-	boolean casting = false;
 	
 	@Override
 	public void create () {
-		walkSheet = new Texture("stickzard.png");
-		spellSheet = new Texture("stickzardThrowFireball.png");
+		player = new Player();
 		litCandleSheet = new Texture("candlelit.png");
 		fireballSheet = new Texture("fireball.png");
 
-		TextureRegion[][] tmp = TextureRegion.split(walkSheet, walkSheet.getWidth() / 2, walkSheet.getHeight() / 2);
-		TextureRegion[][] tmpSpell = TextureRegion.split(spellSheet, spellSheet.getWidth() / 2, spellSheet.getHeight() / 4);
 		TextureRegion[][] tmpCanLit = TextureRegion.split(litCandleSheet,
 				litCandleSheet.getWidth() / 1, litCandleSheet.getHeight() / 5);
 		TextureRegion[][] tmpFireball = TextureRegion.split(fireballSheet, fireballSheet.getWidth(), fireballSheet.getHeight() / 5);
 
-		TextureRegion[] walkFrames = new TextureRegion[4];
-		TextureRegion[] spellFrames = new TextureRegion[8];
 		TextureRegion[] litCandleFrames = new TextureRegion[5];
 		TextureRegion[] fireballFrames = new TextureRegion[5];
 
 		int index = 0;
-		for (int i = 0; i < 2; i++) {
-			for (int j = 0; j < 2; j++) {
-				walkFrames[index++] = tmp[i][j];
-			}
-		}
-
-		index = 0;
-		for (int i = 0; i < 4; i++) {
-			for (int j = 0; j < 2; j++) {
-				spellFrames[index++] = tmpSpell[i][j];
-			}
-		}
-
-		index = 0;
 		for (int i = 0; i < 5; i++) {
 			litCandleFrames[index] = tmpCanLit[i][0];
 			fireballFrames[index++] = tmpFireball[i][0];
 		}
 
-
-		walkAnimation = new Animation<TextureRegion>(0.333f, walkFrames);
-		spellAnimation = new Animation<TextureRegion>(0.1f, spellFrames);
 		candleAnimation = new Animation<TextureRegion>(0.1f, litCandleFrames);
 		fireballAnimation = new Animation<TextureRegion>(0.083f, fireballFrames);
 
 		spriteBatch = new SpriteBatch();
 		stateTime = 0f;
-		playerStateTime = 0f;
 		fireStateTime = 0f;
 	}
 
@@ -82,31 +53,16 @@ public class FireballTheGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(.7f, 0.7f, 0.7f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		stateTime += Gdx.graphics.getDeltaTime();
-		playerStateTime += Gdx.graphics.getDeltaTime();
 		fireStateTime += Gdx.graphics.getDeltaTime();
 
-		TextureRegion currentFrame = walkAnimation.getKeyFrame(stateTime, true);
 		TextureRegion currentCandleFrame = candleAnimation.getKeyFrame(stateTime, true);
-		TextureRegion currentSpellFrame = spellAnimation.getKeyFrame(playerStateTime, false);
 		TextureRegion currentFireball = fireballAnimation.getKeyFrame(fireStateTime, true);
 
 		spriteBatch.begin();
 		if (Gdx.input.justTouched()) {
-			casting = true;
-			playerStateTime = 0f;
+			player.castSpell();
 		}
-
-		if (spellAnimation.isAnimationFinished(playerStateTime)) {
-			playerStateTime = 0f;
-			casting = false;
-		}
-
-		if (casting) {
-			player = currentSpellFrame;
-		} else {
-			player = currentFrame;
-		}
-		spriteBatch.draw(player, 50, 50);
+		player.animate(spriteBatch, Gdx.graphics.getDeltaTime());
 		spriteBatch.draw(currentCandleFrame, 400, 50);
 		spriteBatch.end();
 	}
@@ -114,7 +70,7 @@ public class FireballTheGame extends ApplicationAdapter {
 	@Override
 	public void dispose () {
 		spriteBatch.dispose();
-		walkSheet.dispose();
+		player.dispose();
 		litCandleSheet.dispose();
 	}
 }
---
+++ core/src/com/fireball/game/Player.java
@@ -0,0 +1,74 @@
+package com.fireball.game;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class Player {
+    private Animation<TextureRegion> idleAnimation;
+    private Animation<TextureRegion> spellAnimation;
+    private Texture idleSheet;
+    private Texture spellSheet;
+    private float stateTime;
+    private int x = 50, y = 50;
+    private boolean isCasting;
+
+    public Player() {
+        idleSheet = new Texture("stickzard.png");
+        spellSheet = new Texture("stickzardThrowFireball.png");
+
+        TextureRegion[][] tmp = TextureRegion.split(idleSheet, idleSheet.getWidth() / 2, idleSheet.getHeight() / 2);
+        TextureRegion[][] tmpSpell = TextureRegion.split(spellSheet, spellSheet.getWidth() / 2, spellSheet.getHeight() / 4);
+
+        TextureRegion[] idleFrames = new TextureRegion[4];
+        TextureRegion[] spellFrames = new TextureRegion[8];
+
+        int index = 0;
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                idleFrames[index++] = tmp[i][j];
+            }
+        }
+
+        index = 0;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 2; j++) {
+                spellFrames[index++] = tmpSpell[i][j];
+            }
+        }
+
+        idleAnimation = new Animation<TextureRegion>(0.333f, idleFrames);
+        spellAnimation = new Animation<TextureRegion>(0.1f, spellFrames);
+
+        isCasting = false;
+        stateTime = 0f;
+    }
+
+    public void castSpell() {
+        isCasting = true;
+        stateTime = 0f;
+    }
+
+    public void animate(SpriteBatch spriteBatch, float deltaTime) {
+        stateTime += deltaTime;
+
+        // updates
+        if (isCasting && spellAnimation.isAnimationFinished(stateTime)) {
+            isCasting = false;
+            stateTime = 0f;
+        }
+
+        // Actual animation
+        if (isCasting) {
+            spriteBatch.draw(spellAnimation.getKeyFrame(stateTime, false), x, y);
+        } else {
+            spriteBatch.draw(idleAnimation.getKeyFrame(stateTime, true), x, y);
+        }
+    }
+
+    public void dispose() {
+        idleSheet.dispose();
+        spellSheet.dispose();
+    }
+}
